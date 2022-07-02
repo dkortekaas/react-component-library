b2d1d3b3c0cae2b1e6a57fae8b638bb49d515b59
Dennis Kortekaas <denniskortekaas@MBP-van-Dennis.home>
2022-07-02 17:29:01 +0200

    initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface IButtonProps {
+    label: string
+}
+
+const Button = (props: IButtonProps) => {
+  return (
+    <button>{props.label}</button>
+  )
+}
+
+export default Button
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+
+import { default as Buttona } from "./Button";
